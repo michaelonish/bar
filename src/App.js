d7dec80b7b60f07f80e82a83ce8ee7d319f94ddc
Michael Onishchenko <onishchenkomike@yahoo.com>
2023-08-23 12:58:12 -0700

    added functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
-import { Grid } from "@mui/material";
 import './App.css';
 import { DepartmentList } from "./component/Departments";
 import logo from './logo.jpg';
 
-
 function App() {
-  console.log(logo);
-
   return (
-  <>
-  <div>
-  <div class = "cont">
-  <img src={logo} alt="LOGO" height={50} width={100}  />
-  </div>
-
-    <Grid>
-    <DepartmentList />
-    </Grid>
-      
- </div>
-  </>
+    <div className="container">
+      <img src={logo} alt="LOGO" height={50} width={100} />
+      <DepartmentList />
+    </div>
   );
 }
 
-      
 export default App;
--- src/component/Departments.jsx
+++ src/component/Departments.jsx
@@ -2,14 +2,14 @@ import { Autocomplete, TextField } from "@mui/material";
 import React from "react";
 import logo from './logo.jpg';
 
- 
+
 export const DepartmentList = () => {
-  const departments = ["Order", "Logistics", "Sustainability", "Packaging", "Accounting", "Pharmacy"] ;
+  const departments = ["Order", "Logistics", "Sustainability", "Packaging", "Accounting", "Pharmacy"];
   console.log(logo);
 
   return (
     <Autocomplete
-      style={{ margin: "auto", width: "50%" }}
+      style={{ width: "50%" }}
       img src={logo} alt="LOGO" height={50} width={100}
       options={departments}
       renderInput={(params) => (
